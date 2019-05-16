e9f9d2624d70d2db307312d61c6a6af962416719
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-16 17:20:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
-    const h1 = document.createElement('h2')
+    const h1 = document.createElement('h1')
+    h1.innerHTML = `<h1>${book.title}</h1>`
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
